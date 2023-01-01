6e9b81ce5b3d97a64b86b896da98b8bd02a51336
Pedro Morante <pedro.morante@outlook.com>
2023-01-01 13:30:54 -0500

    Add auto-migrations

--- API/Program.cs
+++ API/Program.cs
@@ -26,6 +26,21 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using(var scope = app.Services.CreateScope()){
+    var services = scope.ServiceProvider;
+    var loggerFactory = services.GetRequiredService<ILoggerFactory>();
+    try
+    {
+        var context = services.GetRequiredService<TiendaContext>();
+        await context.Database.MigrateAsync();
+    }
+    catch (Exception ex)
+    {
+        var logger = loggerFactory.CreateLogger<Program>();
+        logger.LogError(ex, "Ocurrió un error durante la migración");
+    }
+}
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
