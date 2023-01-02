16018ff1acf5af125248b2cf7c7616dabc6a7f88
Pedro Morante <pedro.morante@outlook.com>
2023-01-02 13:17:44 -0500

    Add Configuration folder

--- Core/Entities/Marca.cs
+++ Core/Entities/Marca.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-
-namespace Core.Entities
-{
+namespace Core.Entities;
     public class Marca
-    {
-        public int Id { get; set; }
-        public string Nombre { get; set; }
-        public ICollection<Producto> Productos { get; set; }
-    }
+{
+public int Id { get; set; }
+public string Nombre { get; set; }
+public ICollection<Producto> Productos { get; set; }
 }
--- Infraestructure/Data/TiendaContext.cs
+++ Infraestructure/Data/TiendaContext.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Core.Entities;
 using Microsoft.EntityFrameworkCore;
@@ -14,5 +15,12 @@ namespace Infraestructure.Data
             
         }
         public DbSet<Producto> Productos { get; set; }
+        public DbSet<Marca> Marcas { get; set; }
+        public DbSet<Categoria> Categorias { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder){
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
     }
 }
